60249a1cc73339a40191e68997cdb3c049045318
Christopher Bednar <bednar.christopher@gmail.com>
2014-04-06 05:55:56 -0400

    work on schedule

--- application/views/dash.php
+++ application/views/dash.php
@@ -1,1 +1,15 @@
 <h1>My Schedule</h1>
+
+<?php 
+/*
+echo isAvail($shift, $user);
+echo isAvail($shift, $user);
+echo isAvail($shift, &user);
+*/
+$shifts = $this->shifts_model->get_needed_shifts();
+genSchedule($shifts);
+echo $complete_shifts;
+?>
+
+
+
